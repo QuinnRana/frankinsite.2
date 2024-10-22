63da3153e2dd9d6cf61b76b132fa7d8f18412ce5
Kyle Sorrels <ksorrels@boatsetter.com>
2024-10-21 23:14:38 -0400

    Init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Frankensite server
+
+This repo hold the nodejs server for frankensite
+
+
+## usage
+```
+npm install 
+node server.js
+```
+
+with pm2
+
+```
+pm2 start server.js --name frankenserver
+```
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 8121;
+
+// Middleware to parse JSON and URL-encoded form data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Endpoint to handle form submission
+app.post('/submit', (req, res) => {
+    const { name, id } = req.body;
+
+    // Log form data to a file
+    const logData = `Name: ${name}, StudentId: ${id} \n`;
+    const logFilePath = path.join(__dirname, 'public_html/form-submissions.log');
+
+    fs.appendFile(logFilePath, logData, (err) => {
+        if (err) {
+            console.error('Error writing to log file', err);
+            return res.status(500).send('Internal Server Error');
+        }
+
+        console.log('Form data logged successfully');
+        res.redirect('http://quinnrana.com/frankensite.html');
+
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- VISUAL --                                                                                               36        1,1           All
